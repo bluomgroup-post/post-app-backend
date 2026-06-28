ac8ab4f7419bf31fcf793dfe0c88264c0a696e36
bluomgroup-post <bluomgroup@gmail.com>
2026-06-28 15:59:54 +0530

    Delete src/src directory

--- src/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PostApp from './PostApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PostApp />
-  </React.StrictMode>
-)
